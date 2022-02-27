ac903e436f40fba3ef623340fa0ea718acfc324f
Kelsi Lane <kelsilane91@gmail.com>
2022-02-26 21:41:14 -0600

    lower case generator

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,20 +1,38 @@
-import { Button, Paper } from "@mui/material";
-import React, { useState } from "react";
+import { Button, Paper, TextField } from "@mui/material";
+import React, { BaseSyntheticEvent, useState } from "react";
 import { passwordGenerator } from "../passwordGenerator";
 
 export const Form = () => {
-  const [textValue, setTextValue] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordLength, setPasswordLength] = useState<number>(1);
 
   const handleSubmit = () => {
-    setTextValue(passwordGenerator());
-    console.log(textValue);
+    setPassword(passwordGenerator(passwordLength));
   };
-  const handleReset = () => setTextValue("");
+
+  const handleLengthChange = (event: BaseSyntheticEvent) => {
+    setPasswordLength(event.target.value);
+  };
+
+  const handleReset = () => {
+    setPasswordLength(1);
+    setPassword("");
+  };
 
   return (
     <Paper>
-      <h1>{textValue}</h1>
-
+      <h1>{password}</h1>
+      <TextField
+        id="outlined-number"
+        label="Number"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        InputProps={{ inputProps: { min: 1 } }}
+        onChange={handleLengthChange}
+        value={passwordLength}
+      />
       <Button onClick={handleSubmit}>Submit</Button>
       <Button onClick={handleReset}>Reset</Button>
     </Paper>
--- src/passwordGenerator.ts
+++ src/passwordGenerator.ts
@@ -1,13 +1,29 @@
-export const passwordGenerator = () => {
-  const lowerCaseLetter = getRandomLowerLetter();
-  const upperCaseLetter = getRandomUpperLetter();
-  return lowerCaseLetter.concat(upperCaseLetter);
+// https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+
+export const passwordGenerator = (passwordLength: number) => {
+  let password = "";
+  let passArray = [];
+  for (let i = 0; i <= passwordLength; i++) {
+    let lower = getRandomLowerLetter();
+    passArray.push(lower);
+  }
+  password = passArray.join("");
+  return password;
 };
 
+// 97 - 122
 const getRandomLowerLetter = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
 
+// 65 - 90
 const getRandomUpperLetter = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+// hardcoded list for now
+const getRandomSymbol = () => {
+  const symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  const index = Math.floor(Math.random() * symbolList.length);
+  return symbolList[index];
 };
